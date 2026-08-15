e669da13e13b8895e34364719b82567776bf4564
discodevl <dcutro@disroot.org>
2026-08-15 20:31:28 +0200

    exerc

--- exercises/02_basic_calculator/07_for/src/lib.rs
+++ exercises/02_basic_calculator/07_for/src/lib.rs
@@ -1,6 +1,10 @@
 // Rewrite the factorial function using a `for` loop.
 pub fn factorial(n: u32) -> u32 {
-    todo!()
+    let mut acc = 1;
+    for el in 1..=n {
+        acc *= el;
+    }
+    acc
 }
 
 #[cfg(test)]
